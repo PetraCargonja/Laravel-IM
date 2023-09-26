951ebb4b31e4c0298777598ce9a21b2af65ea0a9
Ivan <ivan.mandic@giscloud.com>
2023-09-26 21:02:56 +0200

    Vjezbe i predavanje varijable i konstante

--- index.php
+++ index.php
@@ -1,21 +1,30 @@
 <?php
 
-require_once 'app.php';
-require_once 'app.php';
+// integer
+$a = 10;
 
-// expressions
-# this is a comment
-/*
- Multi line comment
-*/
-$a = $b = foo();
+// float
+$b = 10.5;
+var_dump(0.1 + 0.2);
+var_dump(0.1 + 0.2 == 0.3);
 
-echo $a, "\n";
-echo $b, "\n";
+// string
+$age = 20;
+$name = "John Doe";
 
-function foo()
-{
-    $c;
-    
-    return 5;
-}
+echo "My name is $name and I am $age years old.\n";
+
+// boolean
+$flag = true;
+$anotherFlag = false;
+
+$isOfAge = $age >= 18;
+
+var_dump($isOfAge);
+var_dump((bool) '0');
+
+// null
+$test = null;
+
+var_dump($test);
+var_dump(null === 0);
