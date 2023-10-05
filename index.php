d4fce960652a10f2acf1d4d7121bf8021e4cb161
Ivan <ivan.mandic@giscloud.com>
2023-10-05 19:41:42 +0200

    Petlje

--- index.php
+++ index.php
@@ -1,28 +1,37 @@
 <?php
 
-switch (date('N')) {
-    case 1:
-        echo "Danas je ponedjeljak\n";
+$people = ['Marko', 'Ivan', 'Jure', 'Ana', 'Iva', 'Luka', 'Ivona', 'Eva', 'Ivo'];
+
+foreach ($people as $value) {
+    echo "Polaznik: {$value}\n";
+}
+
+for ($i=0; $i < count($people); $i++) {
+    if (strlen($people[$i]) < 4) {
+        continue;
+    }
+    
+    echo "Polaznik: {$people[$i]}\n";
+}
+
+for ($i = 0; $i < 10; $i++) {
+    echo "Hello World\n";
+}
+
+$a = 1;
+
+do {
+    echo "Hello World\n";
+
+    $a++;
+
+    if ($a > 10) {
         break;
-    case 2:
-        echo "Danas je utorak\n";
-        break;
-    case 3:
-        echo "Danas je srijeda\n";
-        break;
-    case 4:
-        echo "Danas je četvrtak\n";
-        break;
-    case 5:
-        echo "Danas je petak\n";
-        break;
-    case 6:
-        echo "Danas je subota\n";
-        break;
-    case 7:
-        echo "Danas je nedjelja\n";
-        break;
-    default:
-        echo "Nepoznat dan\n";
-        break;
+    }
+} while (true);
+
+while ($a <= 10) {
+    echo "Hello World\n";
+
+    $a++;
 }
