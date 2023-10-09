0e57a4d54d5dcd7b635405db5eb59a888d65f765
Ivan <ivan.mandic@giscloud.com>
2023-10-09 20:10:31 +0200

    Argumenti funkcije

--- index.php
+++ index.php
@@ -1,10 +1,51 @@
 <?php
 
-function getHelloWorld()
+function echoName(string $name)
 {
-    return "Hello World!";
+    echo "My name is $name\n";
 }
 
-$foo = getHelloWorld();
+function getSum(int $a, int $b)
+{
+    $sum = $a + $b;
 
-echo $foo, "\n";
+    return $sum;
+}
+
+// pass by value by default
+function getSquared(int $a)
+{
+    $a = $a * $a;
+
+    return $a;
+}
+
+// default value
+function makeCoffee(string $type = 'espresso')
+{
+    echo "Making a cup of $type.\n";
+}
+
+makeCoffee('cappuccino');
+makeCoffee();
+
+function makeYogurt($container = 'cup', $flavour = 'natural')
+{
+    echo "Making a $container of $flavour yogurt.\n";
+}
+
+// named arguments
+makeYogurt(flavour: 'blueberry'); // works as expected
+
+function sum(...$numbers)
+{
+    $sum = 0;
+
+    foreach ($numbers as $number) {
+        $sum += $number;
+    }
+
+    return $sum;
+}
+
+echo sum(1, 2, 3, 4, 50, 100), "\n"; // 3
