f3243b8496b9015d3eac679ca1823edbff7f13f8
Ivan <ivan.mandic@giscloud.com>
2023-10-19 18:42:39 +0200

    Upload fajlova vjezba

--- app.php
+++ app.php
@@ -10,11 +10,32 @@ if ($file['error'] !== UPLOAD_ERR_OK) {
     die('Došlo je do greške prilikom prijenosa datoteke!');
 }
 
-if ($file['type'] !== 'text/plain') {
-    die('Datoteka nije txt!');
+$mimeTypes = [
+    'png' => 'image/png',
+    'jpe' => 'image/jpeg',
+    'jpeg' => 'image/jpeg',
+    'jpg' => 'image/jpeg',
+    'gif' => 'image/gif',
+    'bmp' => 'image/bmp',
+    'ico' => 'image/vnd.microsoft.icon',
+    'tiff' => 'image/tiff',
+    'tif' => 'image/tiff',
+    'svg' => 'image/svg+xml',
+    'svgz' => 'image/svg+xml'
+];
+
+if (!in_array($file['type'], $mimeTypes)) {
+    die('Datoteka nije slika!');
 }
 
 $uploadDirectory = __DIR__ . '/uploads/';
+
+if (!file_exists($uploadDirectory)) {
+    if (!mkdir($uploadDirectory)) {
+        die('Došlo je do greške prilikom kreiranja direktorija!');
+    }
+}
+
 $uploadFileName = $uploadDirectory . basename($file['name']);
 
 if (file_exists($uploadFileName)) {
