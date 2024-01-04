8a12083fd620a9ca9e30aca0d2365dbf837a41da
Ivan <ivan.mandic@giscloud.com>
2024-01-04 19:23:40 +0100

    Add routing

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "foo";
+use App\Core\Application;
+
+define('ROOT', realpath(__DIR__ . '/..'));
+
+require_once ROOT . '/vendor/autoload.php';
+require_once ROOT . '/config/routes.php';
+
+$application = new Application();
+$application->run();
